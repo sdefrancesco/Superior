5fed53f195ba3ebb6f1533296fc9037485a53971
sdefrancesco <skaterbastian@aol.com>
2020-10-15 16:43:30 -0400

    add email

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const Vimeo = require('vimeo').Vimeo
 const client = new Vimeo("5f8655ce645e892ffab88c703859aab091301ed3", "sJblbrDHuIlHRViPqKnRgC5a3t30HcRWpz359vxmj0vA/tusgNoO11vnzj+dZKZiQGHvXfY0IREv/Jw23J9qiKnmVoaKNyCNwdKymFPmfBQdOUu8iRQxSrQNZOLFESH0", "17fb45a5043a9a5e10b621716dc47af9");
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
 const hbs = require('hbs')
 const breadcrumb = require('express-url-breadcrumb')
 const app = express()
@@ -10,6 +12,16 @@ app.use(express.static('./public'))
 app.use(breadcrumb())
 
 app.set('view engine', hbs)
+
+//body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+ 
 
 // app.use(express.static('public'))
 
@@ -86,6 +98,54 @@ app.get('/about', breadcrumb(), (req, res)=> {
     res.render('about.hbs', {
         title: 'About Us'
     })
+})
+
+//add email
+app.post('/send-email', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+
+    const output = `
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${req.body.firstname} ${req.body.lastname}</li>
+            <li>date: ${req.body.date}</li>
+            <li>Location: ${req.body.location}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Event Type: ${req.body.partytype}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${req.body.message}</p>
+    `
+    // create reusable transporter object using the default SMTP transport
+ let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    requireTLS: true,
+    auth: {
+      user: 'superiorcreativitynyc@gmail.com', // generated ethereal user
+      pass: 'Dirtymexican93', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let mailOptions = {
+    from: '"Nodemailer Contact" <superiorcreativitynyc@gmail.com>', // sender address
+    to: 'sdefrancesco37@gmail.com', // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+      if(err) {
+          return console.log(err)
+      }
+      console.log('message sent', info.messageId)
+      res.redirect('/')
+  })
 })
 
 const port = process.env.PORT || 3000;
